27d9f3efe76493e74c8f50052a496fc3e0272eec
Andrew Cashman <andrewcashman@Andrews-MacBook-Pro.local>
2018-03-15 08:04:53 -0700

    Added 5 minute javascript ping

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,10 +2,10 @@ module DefaultPageContent
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_pade_defaults
+    before_action :set_page_defaults
   end
 
-  def set_pade_defaults
+  def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
     @seo_keywords = "Andrew Cashman portfolio"
   end
